4a96d24c965fc06a02d870185c1817f77862e4a7
Takeru Ohta <phjgt308@gmail.com>
2012-03-04 11:59:08 +0900

    わかちがきついか

--- src/net/reduls/scala/gomoku/bin/Gomoku.scala
+++ src/net/reduls/scala/gomoku/bin/Gomoku.scala
@@ -6,10 +6,23 @@ import scala.io.Source
 
 object Gomoku {
   def main(args:Array[String]): Unit = {
+    if (args.length >= 1 && args(0) == "-wakati")
+      wakati
+    else
+      parse
+  }
+  
+  def wakati: Unit =
+    for(line <- Source.fromInputStream(System.in).getLines) {
+      for(w <- Tagger.wakati(line))
+        print(w + " ")
+      println("")
+    }
+
+  def parse: Unit = 
     for(line <- Source.fromInputStream(System.in).getLines) {
       for(m <- Tagger.parse(line))
         println(m.surface+"\t"+m.feature)
       println("EOS")
     }
-  }
 }
