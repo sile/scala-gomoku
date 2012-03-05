b621046943ea428539211e848c07a872dc63c366
Takeru Ohta <phjgt308@gmail.com>
2012-03-05 15:19:18 +0900

    連接コストを保持する配列を二次元から一次元に変更

--- src/net/reduls/scala/gomoku/dic/Matrix.scala
+++ src/net/reduls/scala/gomoku/dic/Matrix.scala
@@ -1,15 +1,15 @@
 package net.reduls.scala.gomoku.dic
 
 object Matrix {
-  private val matrix =
+  private val (matrix, leftNum) =
     Util.withDictionaryDataWithCount("matrix.bin") {
       (in, leftNum) =>
         val rightNum = in.readInt
-        val matrix = Array.ofDim[Short](rightNum, leftNum)
+        val matrix = new Array[Short](rightNum * leftNum)
         for(l <- 0 until leftNum; r <- 0 until rightNum)
-          matrix(r)(l) = in.readShort
-        matrix
+          matrix(r*leftNum + l) = in.readShort
+        (matrix, leftNum)
     }
   
-  def linkCost(leftId:Short, rightId:Short): Short = matrix(rightId)(leftId)
+  def linkCost(leftId:Short, rightId:Short): Short = matrix(rightId*leftNum + leftId)
 }
